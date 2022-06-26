b63c08bf0c9c020669419b771b4bbf8e89b454a4
develad <develadb@gmail.com>
2022-06-26 16:03:21 +0300

    adding routes in index.js  and functions in utils.js

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require('express')
 const {baseURLs} = require('./config')
-const {getData, filterToBuyPrice} = require('./utils')
+const {filterToBuyPrice,allitems,scarpeData} = require('./utils')
 
 const app = express()
 
 const PORT = process.env.PORT || 5000
 
-app.get('/',async (req,res)=>{
+app.get('/allitems', async(req,res)=>{
     
-    const itemsScrapted = baseURLs.map(item=>getData(item.baseURL,item.buyPrice))
+    const itemsScrapted =  scarpeData(baseURLs)
+    const allItemsScrapted = await allitems(itemsScrapted)
+    res.status(200).json(allItemsScrapted)
+})
+app.get('/buyitems',async (req,res)=>{
+    
+    const itemsScrapted = scarpeData(baseURLs)
     const itemsInBuyPriceRange = await filterToBuyPrice(itemsScrapted)
     res.status(200).json(itemsInBuyPriceRange)
 })
--- utils.js
+++ utils.js
@@ -1,7 +1,7 @@
 const axios = require('axios').default
 const cheerio = require('cheerio')
 const {format} = require('date-fns')
-const {reqHeaders} = require('./config')
+const {reqHeaders, baseURLs} = require('./config')
 
 
 const getData = async(baseURL,buyPrice)=>{
@@ -26,6 +26,12 @@ const getData = async(baseURL,buyPrice)=>{
     })
 }
 
+const allitems = (itemsArr) =>{
+    return Promise.allSettled(itemsArr).then(data=>{
+        const items = data.map(item=>item.value)
+        return items
+})
+}
 const filterToBuyPrice = (itemsArr) =>{
     return Promise.allSettled(itemsArr).then(data=>{
         const items = data.map(item=>item.value)
@@ -34,7 +40,13 @@ const filterToBuyPrice = (itemsArr) =>{
 })
 }
 
+const scarpeData = (baseURLs) =>{
+   return baseURLs.map(item=>getData(item.baseURL,item.buyPrice))
+} 
+
 module.exports = {
     getData,
-    filterToBuyPrice
+    filterToBuyPrice,
+    allitems,
+    scarpeData
 }
